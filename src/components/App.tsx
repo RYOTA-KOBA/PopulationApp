cc68a268f3f74f7cb193e9001d6942686ece5708
RYOTA <kobaryo04ysh@icloud.com>
2021-03-24 21:06:15 +0900

    first commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../styles/App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,13 +14,13 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+      <h1>Hello</h1>
     </div>
   );
-}
+};
 
 export default App;
