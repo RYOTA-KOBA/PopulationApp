ea66ea9b9494c93905135f56648b3209c9e16821
RYOTA <kobaryo04ysh@icloud.com>
2021-03-25 13:13:57 +0900

    feat: とある市町村の総人口no 配列をイテレートして表示

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/App.css';
 
 const api_key = process.env.REACT_APP_API_KEY;
 
+type P = {
+  year: number;
+  value: number;
+};
+
 const App: React.FC = () => {
+  const [populations, setPopulations] = useState<P[]>([]);
+
   useEffect(() => {
     fetch(
       'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=11362&prefCode=11',
@@ -15,10 +22,11 @@ const App: React.FC = () => {
       }
     )
       .then((res) => {
-        const result = res.json();
-        result.then((data) => {
-          console.log(data.result.data);
-        });
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data.result.data[0].data);
+        setPopulations(data.result.data[0].data);
       })
       .catch((error) => {
         console.log(error);
@@ -28,6 +36,16 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <h1>api sample</h1>
+      <div>
+        {populations.map((population: P, index: number) => (
+          <div key={index}>
+            {console.log(population)}
+            <p>
+              {population.year} : {population.value}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
