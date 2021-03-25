d30d1d083475223a003c497270ee734dce21d32f
RYOTA <kobaryo04ysh@icloud.com>
2021-03-25 14:10:48 +0900

    feat: highchartsを追加し、demoデータでグラフ描画

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/App.css';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 const api_key = process.env.REACT_APP_API_KEY;
 
@@ -56,6 +58,42 @@ const App: React.FC = () => {
       });
   };
 
+  const options = {
+    title: {
+      text: '総人口推移',
+    },
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false,
+        },
+        pointStart: 2010,
+      },
+    },
+    series: [
+      {
+        name: 'Installation',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+      },
+      {
+        name: 'Manufacturing',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+      },
+      {
+        name: 'Sales & Distribution',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
+      },
+      {
+        name: 'Project Development',
+        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
+      },
+      {
+        name: 'Other',
+        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+      },
+    ],
+  };
+
   useEffect(() => {
     getPopulationData();
     getPrefectures();
@@ -64,24 +102,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <h1>api sample</h1>
-      <div>
-        {prefectures.map((prefecture: PF, index: number) => (
-          <div key={index}>
-            <p>
-              {prefecture.prefCode} : {prefecture.prefName}
-            </p>
-          </div>
-        ))}
-      </div>
-      <div>
-        {populations.map((population: P, index: number) => (
-          <div key={index}>
-            <p>
-              {population.year} : {population.value}
-            </p>
-          </div>
-        ))}
-      </div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
   );
 };
